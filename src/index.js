2d25e88e768e9f4e6c9501a5b730da4a53f2900f
Janio Melo <janiobmelo@gmail.com>
2019-05-01 09:18:02 -0300

    exportar App para teste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
@@ -10,3 +10,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+
+export { default as App } from "./App";
